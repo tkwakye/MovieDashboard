e86a9530dddbc502144e86d71bae579471c75063
Tiffanie Kwakye <127369110+tkwakye@users.noreply.github.com>
2024-04-19 11:38:46 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
-from dash import dcc, html, dash_table
-from dash.dependencies import Input, Output
+from dash import dcc, html, dash_table, Input, Output
+#from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
