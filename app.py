559b2c63ff82bcad6bd6289e7a1c8a86311f7a0b
Tiffanie Kwakye <127369110+tkwakye@users.noreply.github.com>
2024-04-21 03:06:32 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
-import statsmodels.api as sm
+
 
 df_movies = pd.read_csv("data/cleaned_movies_data_transformed.csv")
 df_moviesDT = pd.read_csv("data/cleaned_movies_table_data.csv")
